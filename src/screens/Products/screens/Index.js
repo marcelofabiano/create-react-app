afa112dffd9820bf89558b7aec42d4217b6b837e
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2018-04-11 17:28:38 -0300

    example products

--- src/screens/Products/screens/Index.js
+++ src/screens/Products/screens/Index.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-  <div className="page">
-    <h1>Produtos</h1>
-  </div>
-)
+import api from '../../../api'
+
+class Products extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+    this.loadData = this.loadData.bind(this)
+  }
+
+  loadData() {
+    api.get('/products').then((res) => (
+      this.setState({
+        products: res.data
+      })
+    )).catch((res) => (
+      console.log(res.error)
+    ))
+  }
+
+  componentDidMount() {
+    this.loadData()
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <h1>Produtos</h1>
+        <p>{JSON.stringify(this.state.products)}</p>
+      </div>
+    )
+  }
+}
+
+export default Products
